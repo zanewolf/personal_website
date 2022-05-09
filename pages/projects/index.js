a6341011c20793dc9130b3fe7593d5a8e0be666a
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-09 12:58:55 -0400

    updated env variables 4

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,7 +6,7 @@ export async function getStaticProps(){
 
     const client = createClient({
         space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     })
 
     const res = await client.getEntries({content_type:'projects'})
