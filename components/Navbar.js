972af38509854a8b20ed92ac4fabc548cce1d33c
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-12 23:57:29 -0400

    fixing link 2

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,7 +13,9 @@ function MobileNav({open, setOpen}) {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen bg-black transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> {/*logo container*/}
-                <a className="text-xl font-semibold" href="/">zane wolf</a>
+                <Link href="/">
+                    <a className="text-xl font-semibold" >zane wolf</a>
+                </Link>
             </div>
             <div className="flex flex-col justify-center">
                 {menuData.map((navLink,i)=>{
