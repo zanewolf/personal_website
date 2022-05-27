333ffe402c2d2d412ab30dcb554db518d8719eac
zanewolf <zwolf.mlxvi@gmail.com>
2022-05-27 08:49:45 -0400

    fixing project menu color bug hopefully pt10 - removed @emotion

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,35 +1,34 @@
-import React, {useState,Fragment} from 'react'
+import React from 'react'
 import ProjectCard2 from "../../components/ProjectCard2";
 import {fetchMedia} from "../../utils/ContentfulAPI";
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 const categories = [{
     name: 'All',
     color: 'text-primary',
     value: '10'
-},{
+}, {
     name: 'Data Visualization',
     color: 'text-secondary-200',
     value: '10'
-},{
+}, {
     name: 'Web Design',
     color: 'text-secondary-300',
     value: '10'
-},{
+}, {
     name: 'Science',
     color: 'text-secondary-400',
     value: '10'
 }]
 
 
-export async function getStaticProps(){
+export async function getStaticProps() {
 
     let content = fetchMedia('projects')
-        .then((projectsFetched)=> projectsFetched)
-        .catch((error)=> console.log(error))
+        .then((projectsFetched) => projectsFetched)
+        .catch((error) => console.log(error))
 
     return content
 }
@@ -37,24 +36,23 @@ export async function getStaticProps(){
 export default function ProjectPage({content}) {
 
     const [selectedCategory, setSelectedCategory] = React.useState(categories[0].name)
-    const [displayContent,setDisplayContent] = React.useState(content)
+    const [displayContent, setDisplayContent] = React.useState(content)
 
-    React.useEffect(()=>{
-        if (selectedCategory=='All'){
+    React.useEffect(() => {
+        if (selectedCategory == 'All') {
             setDisplayContent(content)
-        } else{
-            setDisplayContent(content.filter(project=>project.fields.categories==selectedCategory))
+        } else {
+            setDisplayContent(content.filter(project => project.fields.categories == selectedCategory))
         }
 
-    },[selectedCategory])
+    }, [selectedCategory])
 
-    const handleSelect=(event)=>{
+    const handleSelect = (event) => {
         // console.log(event)
         setSelectedCategory(event.target.value)
     }
 
-    console.log(categories.filter(cat=>cat.name===selectedCategory)[0].color)
-
+    console.log(categories.filter(cat => cat.name === selectedCategory)[0].color)
 
 
     return (
@@ -62,23 +60,24 @@ export default function ProjectPage({content}) {
             <div className="text-3xl lg:text-4xl  flex flex-row justify-center items-center m-auto mt-16 lowercase">
                 <div className={'projectForm pr-4 pt-2'}>
                     {/*<div>*/}
-                        <FormControl sx={{ m: 1, minWidth: 80 }} className={' border-0 m-0'}>
-                            {/*<InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>*/}
-                            <Select
-                                labelId="demo-simple-select-autowidth-label"
-                                id="demo-simple-select-autowidth"
-                                value={selectedCategory}
-                                onChange={handleSelect}
-                                autoWidth
-                                // label="Project"
-                                className={`!text-3xl lg:!text-4xl !font-bold ${categories.filter(cat=>cat.name===selectedCategory)[0].color} hover:scale-105 `}
-                            >
-                                {categories.map((cat,i)=>{
-                                    // console.log(cat)
-                                    return <MenuItem key={i} value={cat.name} className={`border-bottom-2 border-white ${cat.color}`} >{cat.name}</MenuItem>
-                                })}
-                            </Select>
-                        </FormControl>
+                    <FormControl sx={{m: 1, minWidth: 80}} className={' border-0 m-0'}>
+                        {/*<InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>*/}
+                        <Select
+                            labelId="demo-simple-select-autowidth-label"
+                            id="demo-simple-select-autowidth"
+                            value={selectedCategory}
+                            onChange={handleSelect}
+                            autoWidth
+                            // label="Project"
+                            className={`!text-3xl lg:!text-4xl !font-bold ${categories.filter(cat => cat.name === selectedCategory)[0].color} hover:scale-105 `}
+                        >
+                            {categories.map((cat, i) => {
+                                // console.log(cat)
+                                return <MenuItem key={i} value={cat.name}
+                                                 className={`border-bottom-2 border-white `}>{cat.name}</MenuItem>
+                            })}
+                        </Select>
+                    </FormControl>
                     {/*</div>*/}
                 </div>
                 <span className={'font-extralight'}>projects</span>
@@ -92,7 +91,7 @@ export default function ProjectPage({content}) {
             </div>
             <div className="projectCards flex flex-wrap gap-4 mb-16 mt-16 justify-center">
                 {displayContent.map(project =>
-                    <ProjectCard2 key={project.sys.id} project={project} />
+                    <ProjectCard2 key={project.sys.id} project={project}/>
                 )}
             </div>
 
@@ -107,21 +106,22 @@ export default function ProjectPage({content}) {
             {/*    <ProjectCard2 key={project.sys.id} project={project} />*/}
             {/*))}*/}
             <style jsx>{`
-        .project-list {
-        display: flex;
-        flex-flow: row wrap;
-        //gap: 50px;
-        justify-content: center;
-        //background-color: white;
-        //height: 30vh;
-          //display: grid;
-          //grid-template-columns: 1fr 1fr;
-          //grid-gap: 400px 60px;
-        }
-        .projectCards{
-        min-height:50vh
-        }
-      `}</style>
+              .project-list {
+                display: flex;
+                flex-flow: row wrap;
+                //gap: 50px;
+                justify-content: center;
+                //background-color: white;
+                //height: 30vh;
+                //display: grid;
+                //grid-template-columns: 1fr 1fr;
+                //grid-gap: 400px 60px;
+              }
+
+              .projectCards {
+                min-height: 50vh
+              }
+            `}</style>
         </div>
     )
 }
